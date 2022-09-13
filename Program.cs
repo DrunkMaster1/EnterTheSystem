3e9eccf57cffbba4f3b5650e342b5fb29be37438
Глеб Бутяев <iiaiias1992@gmail.com>
2022-09-13 16:03:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Введите имя:");
+    string name1 = Console.ReadLine();
+    Console.WriteLine("Введите пароль:");
+    int password1 = int.Parse(Console.ReadLine());
+
+
+}

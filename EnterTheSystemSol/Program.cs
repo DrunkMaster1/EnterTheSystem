ea43266ee8ffa65d9c27f6d8b8ebd351903ee304
Глеб Бутяев <iiaiias1992@gmail.com>
2022-09-13 16:31:12 +0300

    Сделал алгоритм запроса пароля, после 3-х попыток вводить данные нельзя.

--- EnterTheSystemSol/Program.cs
+++ EnterTheSystemSol/Program.cs
@@ -1,12 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-for (int i = 0; i < 3; i++)
+﻿internal class Program
 {
-    Console.WriteLine("Введите имя:");
-    string name1 = Console.ReadLine();
-    Console.WriteLine("Введите пароль:");
-    int password1 = int.Parse(Console.ReadLine());
+    private static void Main(string[] args)
+    {
+        int pas = 1234;
+        string name = "gggg";
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("Введите имя:");
+            string f = Console.ReadLine();
+            Console.WriteLine("Введите пароль:");
+            int g = int.Parse(Console.ReadLine());
+            if (g == pas && f == name)
+
+            {
+                Console.WriteLine();
+                Console.WriteLine("Enter the System");
+                break;
+            
+            }
+
+        }
+        Console.WriteLine();
+        Console.WriteLine("The number of available tries have been exceeded");
 
 
+
+    }
 }
-Console.Read();
